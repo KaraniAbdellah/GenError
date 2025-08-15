8add3ce12cdc6f7c152e3250e28573fd4e6258a1
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-15 23:05:50 +0100

    docs: add learn folder for note

---
+++ learn/prisma/index.ts
@@ -0,0 +1,13 @@
+// Prisma
+/*
+    Prisma is a next-generation ORM that consists of these tools:
+        Prisma Client: Auto-generated and type-safe query builder for Node.js & TypeScript
+        Prisma Migrate: Declarative data modeling & migration system
+        Prisma Studio: GUI to view and edit data in your database
+
+    npm install express @prisma/client dotenv
+    npx i prisma --> Initialize Prisma 
+*/
+
+
+
