ce1c7460ef9a76e07d0e79df64b8330b8834200a
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-15 23:21:53 +0100

    feat: configure api for GenError

--- app/backend/app.ts
+++ app/backend/app.ts
@@ -1,9 +1,12 @@
 // server start here
-import 'dotenv/config'
+import "dotenv/config";
 
+import express, { Request, Response } from "express";
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Server Running in PORT ${PORT}`);
+});
