9b9cd01e37f23482a2695afe311537f91f08e63c
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-19 13:25:23 +0100

    docs: update docs for prisma

--- app/backend/controllers/user.controller.ts
+++ app/backend/controllers/user.controller.ts
@@ -1,27 +1,30 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
-
 // import types
 import { userType } from "../models/types";
 
 const userClient = new PrismaClient().user;
 
 // Get User
-export const get_user = async (req: Request, res: Response) => {
+export const addUser = async (req: Request, res: Response) => {
   try {
     const newUser: userType = {
       name: "Ahmed",
       email: "a@gmail.com",
       password: "1010292",
     };
-    // Push User to Database 
-    const auth_user = await userClient.create({ // prisma create new user with id, ...
-      data: newUser
+    // Push User to Database
+    const auth_user = await userClient.create({
+      // prisma create new user with id, ...
+      data: newUser,
     });
-
     return res.status(200).send({ NewUser: auth_user });
-  } catch (error: Error) {
-    return res.status(500).send({ message: error.message });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send({ message: error.message });
+    } else {
+      return res.status(500).send({ message: "Unexpected error" + error });
+    }
   }
 };
--- app/backend/routes/user.routes.ts
+++ app/backend/routes/user.routes.ts
@@ -3,12 +3,12 @@
 import express, { Router } from "express";
 
 // Import User Controller
-import { get_user } from "../controllers/user.controller";
+import { addUser } from "../controllers/user.controller";
 
 const user_route: Router = express.Router();
 
 
-user_route.get("/get_user", get_user);
+user_route.post("/addUser", addUser);
 
 
 export default user_route;
