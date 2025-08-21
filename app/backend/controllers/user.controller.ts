04fc7af77ec0c8c2d80d817d8197f84332a20388
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-21 20:42:38 +0100

    fix: fix addUser EndPoint

--- app/backend/controllers/user.controller.ts
+++ app/backend/controllers/user.controller.ts
@@ -13,8 +13,8 @@ export const addUser: RequestHandler = async (req: Request, res: Response) => {
     // const newUser: userType = req.body;
     const newUser: userType = {
       name: "Ahmed",
-      email: "a@gmail.com",
-      password: "1010292",
+      email: "a2@gmail.com",
+      password: "19192002",
     };
 
     // Check if user already login --> we can not push into database
@@ -24,15 +24,26 @@ export const addUser: RequestHandler = async (req: Request, res: Response) => {
       },
     });
     if (user) {
-      return res.status(200).json({message: "User Already Exit"});
+      return res.status(200).json({ user: user });
     }
 
     // Push User to Database
     const auth_user = await userClient.create({
-      // prisma create new user with id, ...
+      // prisma create new user with id (auto), ...
       data: newUser,
+      include: {
+        Sessions: {
+          include: {
+            Prompts: {
+              include: {
+                Output: true
+              }
+            }
+          }
+        }
+      }
     });
-    return res.status(200).send({ NewUser: auth_user });
+    return res.status(200).send({ user: auth_user });
   } catch (error) {
     if (error instanceof Error) {
       return res.status(500).send({ message: error.message });
