2083a5a54f856701b56aafae8e1a054a16822f2f
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-24 23:27:11 +0100

    fix: fix problem of argument of type null

--- app/backend/utils/generateToken.ts
+++ app/backend/utils/generateToken.ts
@@ -2,9 +2,11 @@ import { userType } from "../models/types";
 import jwt from "jsonwebtoken";
 import config from "../config/config";
 
-const generateToken = (user: userType) => {
-  const token: string = jwt.sign(user?.name, config.secret_key, {
-    expiresIn: "100000s",
-  });
+const generateToken = (user: userType | null) => {
+  if (user) {
+    const token: string = jwt.sign(user?.name, config.secret_key, {
+      expiresIn: "100000s",
+    });
+  }
 };
 export default generateToken;
