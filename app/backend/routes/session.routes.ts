6ad91bfe8156f18a92adc12b2c48cf1835022724
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-31 16:26:43 +0100

    feat: remove get Session endpoints becuase we have get User Endpoints that can get all info

--- app/backend/routes/session.routes.ts
+++ app/backend/routes/session.routes.ts
@@ -2,11 +2,10 @@ import express, { Router } from "express";
 import { authMiddlware } from "../middlwares/auth.middlware";
 import { userMiddlware } from "../middlwares/user.middlware";
 // Import User Controller
-import { addSession,getSession } from "../controllers/session.controller";
+import { addSession } from "../controllers/session.controller";
 
 const session_route: Router = express.Router();
 
 session_route.post("/addSession", authMiddlware, userMiddlware, addSession);
-session_route.post("/getSession", authMiddlware, userMiddlware, getSession);
 
 export default session_route;
