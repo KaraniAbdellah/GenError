b73c113964bf43915601fbeb517efc8eed18eee5
KaraniAbdellah <abdellahkarani@gmail.com>
2025-08-31 16:22:41 +0100

    fix: change undefined to null

--- app/backend/controllers/session.controller.ts
+++ app/backend/controllers/session.controller.ts
@@ -15,11 +15,10 @@ export const addSession: RequestHandler = async (
       return res.status(404).send({ message: "user ot found" });
     }
     // Create a Session
-    const newSession: sessionCreateType | undefined = {
+    const newSession: sessionCreateType | null = {
       session_name: req.body.session_name,
-      user_id: user.id
+      user_id: user.id,
     };
-
 
     if (!newSession) {
       return res
@@ -28,7 +27,7 @@ export const addSession: RequestHandler = async (
     }
 
     // Add Session in Database
-    const SessionPush: sessionType | undefined = await sessionClient.create({
+    const SessionPush: sessionType | null = await sessionClient.create({
       data: newSession,
       include: {
         Prompts: {
@@ -44,6 +43,43 @@ export const addSession: RequestHandler = async (
         .send({ message: "can not create session object in database" });
     }
     return res.status(200).send(newSession);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send({ message: error.message });
+    } else {
+      return res.status(500).send({ message: "Unexpected error" + error });
+    }
+  }
+};
+
+export const getSession: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const user: userType | undefined = req.user;
+    if (!user) {
+      return res.status(404).send({ message: "user ot found" });
+    }
+
+    const userSession: sessionType | null = await sessionClient.findUnique({
+      where: {
+        user_id: user.id,
+      },
+      include: {
+        Prompts: {
+          include: {
+            Output: true,
+          },
+        },
+      },
+    });
+
+    if (!userSession) {
+      return res.status(404).send({ message: "Session not found" });
+    }
+
+    return res.status(200).send(userSession);
   } catch (error) {
     if (error instanceof Error) {
       return res.status(500).send({ message: error.message });
--- app/backend/routes/session.routes.ts
+++ app/backend/routes/session.routes.ts
@@ -2,10 +2,11 @@ import express, { Router } from "express";
 import { authMiddlware } from "../middlwares/auth.middlware";
 import { userMiddlware } from "../middlwares/user.middlware";
 // Import User Controller
-import { addSession } from "../controllers/session.controller";
+import { addSession,getSession } from "../controllers/session.controller";
 
 const session_route: Router = express.Router();
 
 session_route.post("/addSession", authMiddlware, userMiddlware, addSession);
+session_route.post("/getSession", authMiddlware, userMiddlware, getSession);
 
 export default session_route;
