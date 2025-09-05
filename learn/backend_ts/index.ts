4cfe34105c7d4c997929fe12a1ca33a0d1d9bffa
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-05 23:19:15 +0100

    why npm i can have errors

--- learn/backend_ts/index.ts
+++ learn/backend_ts/index.ts
@@ -8,3 +8,24 @@
             --> ts-node: TypeScript execution environment for Node.js.
 */
 
+// Why Error Happen in npm i:
+/*
+Usually < 3
+when we fork a project
+we run npm i to install all the dependencies.
+But something strange happens: we see some vulnerability errors,
+yet when we run the project it still works fine, like nothing is wrong.
+
+--> Actually, there are a lot of dependency errors here:
+    // 36 packages are looking for funding → This means 36 authors are asking for donations.
+    //  Warnings → The project uses some old libraries that should be updated.
+    //  18 vulnerabilities → npm checked and found some security issues.
+
+--> Solutions:
+    // We can use: "npm audit fix force"
+
+--> But what audit do exactly:
+
+*/
+
+
