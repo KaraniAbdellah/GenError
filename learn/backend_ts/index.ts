35555b38e5240d416dd7b349d7bc9ad84bad2d4a
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-14 10:15:10 +0100

    docs: add how we can update all packages

--- learn/backend_ts/index.ts
+++ learn/backend_ts/index.ts
@@ -8,30 +8,30 @@
             --> ts-node: TypeScript execution environment for Node.js.
 */
 
-// Why Error Happen in npm i:
+// Why Error Happen in npm install:
 /*
 Usually < 3
 when we fork a project
-we run npm i to install all the dependencies.
+we run npm install to install all the dependencies.
 But something strange happens: we see some vulnerability errors,
 yet when we run the project it still works fine, like nothing is wrong.
 
 --> Actually, there are a lot of dependency errors here:
-    // 36 packages are looking for funding → This means 36 authors are asking for donations.
-    //  Warnings → The project uses some old libraries that should be updated.
-    //  18 vulnerabilities → npm checked and found some security issues.
+    /// 36 packages are looking for funding → This means 36 authors are asking for donations.
+    ///  Warnings → The project uses some old libraries that should be updated.
+    ///  18 vulnerabilities → npm checked and found some security issues.
 
 --> Solutions:
-    // We can use: "npm audit fix force"
+    /// We can use: "npm audit fix force"
 
 --> But what audit do exactly:
-    in runing, audit look at packages and send all of them npm security 
+    in runing, audit look at packages and send all of them to npm security 
     database for check each package is have any vulunarbitl and then audit generate 
-    repport for all them (packages, there vulunarify, version, status [low, modern ,...])
+    repport contain (package, vulnerability, version, status [low, modern ,...])
 
 
 --> for best practcie in big react project:
-    .......
+    https://github.blog/security/supply-chain-security/github-advisory-database-now-powers-npm-audit/
 */
 
 
