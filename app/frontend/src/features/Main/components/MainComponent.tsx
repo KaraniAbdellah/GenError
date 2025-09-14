fa44297b941cd798f9c205495c15096f323387fb
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-14 13:32:52 +0100

    feat: add main components and also Header

---
+++ app/frontend/src/features/Main/components/MainComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainComponent = () => {
+    return (
+        <div>
+            <h1>What is Today's Problem!</h1>
+            <input type="text" />
+        </div>
+    );
+}
+
+export default MainComponent;
