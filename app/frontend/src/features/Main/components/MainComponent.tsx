9a237cfa47724bd35d81b9966f39713fa8e12383
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-16 23:35:43 +0100

    fix: add colors

--- app/frontend/src/features/Main/components/MainComponent.tsx
+++ app/frontend/src/features/Main/components/MainComponent.tsx
@@ -6,12 +6,13 @@ const MainComponent = () => {
       <div className="max-w-2xl w-full space-y-8">
         <div className="relative">
           <div className="relative flex items-center">
-            <textarea
+            <input
+              type="text"
               placeholder="Type your error here..."
-              className="w-full px-6 py-4 pr-14 bg-zinc-700 border border-zinc-600 rounded-xl text-white 
-              placeholder-gray-400 focus:ring-2 focus:ring-green-500 text-lg focus:outline-0"
-            ></textarea>
-            <button className="absolute right-3 p-2 bg-gradient-to-r from-green-500 to-green-500 hover:from-green-600 hover:to-green-600 rounded-lg transition-all duration-200 transform hover:scale-105">
+              className="w-full px-6 py-4 pr-14 bg-zinc-700 border border-green-400 text-white 
+              placeholder-green-400 text-lg focus:outline-0 rounded-sm"
+            />
+            <button className="cursor-pointer absolute right-3 p-2 bg-gradient-to-r from-green-400 to-green-800 rounded-sm">
               <Send className="text-white" size={20} />
             </button>
           </div>
