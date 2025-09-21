bb714b5be46f824b0590ada8bac3d57407e986e3
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-21 22:54:10 +0100

    feat:  start working with shadcn

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,18 +1,21 @@
 import "./index.css";
-import MainSidebarComponent from "./features/Sidebar/components/MainSidebarComponent";
-import MainComponent from "./features/Main/components/MainComponent";
-import Header from "./features/Header/components/Header";
-import Footer from "./features/Footer/components/Footer";
+// import MainSidebarComponent from "./features/Sidebar/components/MainSidebarComponent";
+// import MainComponent from "./features/Main/components/MainComponent";
+// import Header from "./features/Header/components/Header";
+// import Footer from "./features/Footer/components/Footer";
+import Page from "./features/Page";
 
 function App() {
   return (
-    <div className="h-screen bg-zinc-800">
-      <Header></Header>
-      <div className="flex justify-start h-[calc(100vh-61px)]">
+    <div className="h-screen bg-zinc-800 w-full">
+      {/* <Header></Header> */}
+      {/* <div className="flex justify-start h-[calc(100vh-61px)]">
         <MainSidebarComponent></MainSidebarComponent>
         <MainComponent></MainComponent>
       </div>
-      <Footer></Footer>
+      <Footer></Footer> */}
+      <Page></Page>
+      <h1>ss</h1>
     </div>
   );
 }
