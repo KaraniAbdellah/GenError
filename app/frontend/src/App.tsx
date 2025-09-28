e956b458d95afecbde9ed5740bc1c388ad44daf9
KaraniAbdellah <abdellahkarani@gmail.com>
2025-09-28 22:49:09 +0100

    feat: add shadcn template && and start adapt it

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,21 +1,11 @@
 import "./index.css";
-// import MainSidebarComponent from "./features/Sidebar/components/MainSidebarComponent";
-// import MainComponent from "./features/Main/components/MainComponent";
-// import Header from "./features/Header/components/Header";
-// import Footer from "./features/Footer/components/Footer";
-import Page from "./features/Page";
+
+import Page from "./pages/Page";
 
 function App() {
   return (
     <div className="h-screen bg-zinc-800 w-full">
-      {/* <Header></Header> */}
-      {/* <div className="flex justify-start h-[calc(100vh-61px)]">
-        <MainSidebarComponent></MainSidebarComponent>
-        <MainComponent></MainComponent>
-      </div>
-      <Footer></Footer> */}
       <Page></Page>
-      <h1>ss</h1>
     </div>
   );
 }
---
+++ app/frontend/src/pages/Page.tsx
@@ -0,0 +1,41 @@
+import { SidebarLeft } from "@/components/sidebar-left"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Page() {
+  return (
+    <SidebarProvider>
+      <SidebarLeft />
+      <SidebarInset>
+        <header className="bg-background sticky top-0 flex h-14 shrink-0 items-center gap-2">
+          <div className="flex flex-1 items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage className="line-clamp-1">
+                  </BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
