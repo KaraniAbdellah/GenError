cda3685df24f423871da411afaa1cefb1a5ddf9e
KaraniAbdellah <abdellahkarani@gmail.com>
2025-10-05 19:59:47 +0100

    feat: define routes

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,27 @@
 import "./index.css";
+import About from "./pages/About";
 import Page from "./pages/Page";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Trachs from "./pages/Trachs";
+import Help from "./pages/Help";
+import UpgradeToPro from "./pages/UpgradeToPro";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
 
 function App() {
   return (
     <div className="h-screen bg-zinc-800 w-full">
-      <Page></Page>
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Page />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/trachs" element={<Trachs />}></Route>
+            <Route path="/help" element={<Help />}></Route>
+            <Route path="/upgrade" element={<UpgradeToPro />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/account" element={<Account />}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
