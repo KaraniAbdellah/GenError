725464e2f0655ae0cf6f4ca87fd410c43f03fa7f
KaraniAbdellah <abdellahkarani@gmail.com>
2025-10-11 00:37:03 +0100

    feat: functio return

--- app/frontend/src/services/utils/RenderMessageCardByFlag.ts
+++ app/frontend/src/services/utils/RenderMessageCardByFlag.ts
@@ -1,4 +1,7 @@
 
-function RenderMessageCardByFlag() {
+
+// This Function Should Be Return Object That Contain Color for Each Message and Toast Code
+
+function RenderMessageCardByFlag(messages) {
 
 }
