5aeeb0ababaf480ac4b77790e7e65f47a5648223
KaraniAbdellah <abdellahkarani@gmail.com>
2025-10-13 21:03:29 +0100

    feat: add new constante is Session Data Demon

---
+++ app/frontend/src/constant/sessionDataDemo.ts
@@ -0,0 +1,22 @@
+import { Session } from "@/types/UserTypes";
+
+const sessionDataDemo: Session = {
+  id: "68e3a6b7b3d81f9ae7870f35",
+  session_name: "Session 1",
+  user_id: "68e3a699b3d81f9ae7870f33",
+  Prompts: [
+    {
+      id: "68e3a6eab3d81f9ae7870f39",
+      prompt_text: "Give Me A Custom Error Here",
+      session_id: "68e3a6c2b3d81f9ae7870f36",
+      Output: {
+        id: "68e3a775321fd5e8d6c64b4c",
+        messages: [],
+        explanation: "Hello",
+        prompt_id: "68e3a6eab3d81f9ae7870f39",
+      },
+    },
+  ],
+};
+
+export default sessionDataDemo;
---
+++ app/frontend/src/context/SessionContext.tsx
@@ -0,0 +1,9 @@
+// This Context Hold Actually Session
+import { createContext } from "react";
+import { Session } from "@/types/UserTypes";
+
+
+
+const SessionContext = createContext<Session | null>(null);
+
+export default SessionContext;
