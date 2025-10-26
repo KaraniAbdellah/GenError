9347f3f5e03b02e9b72fd3de4f7d6e1631b1aa77
KaraniAbdellah <abdellahkarani@gmail.com>
2025-10-26 21:09:16 +0100

    rm learn

--- learn/prisma/errors.ts
+++
@@ -1,7 +0,0 @@
-// So In Backend With Prisma And Ts We have Buit-in types in prisma
-// so we can use this type without create another types with ts
-/*
-    import { User as prismaUserType, } from "@prisma/client";
-    export type userType = Omit<prismaUserType, "id">;
-*/
-
